835135f2b288defaeb74487ab4513d8ed65fe888
Fabio Camargo <fabiorcamargo@gmail.com>
2023-05-22 09:36:12 -0300

    page type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
     
             $this->call([
-                StatesSeed::class,
-                CitiesSeed::class,
+                PageTypeSeeder::class
             ]);
       
     }
---
+++ database/seeders/PageTypeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PageType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PageTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        PageType::insert([
+            'name' => 'Pré Militar',
+            'name' => 'Bancário',
+            'name' => 'ENCCEJA',
+            'name' => 'CPA10',
+        ]);
+    }
+}
