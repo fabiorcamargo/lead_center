9ed48112c7864c13d3f9699727b0a2b6b3914c7a
Fabio Camargo <fabiorcamargo@gmail.com>
2023-06-07 21:18:55 -0300

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "h94ax2avlp");
+            })(window, document, "clarity", "script", "hgeu60n4d7");
         </script>
         @endif
 
